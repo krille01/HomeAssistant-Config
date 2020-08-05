5b81b1b8698388dfbf44591f390b6abdb0943300
krille01 <kfosserud@gmail.com>
2020-08-05 16:31:56 +0200

    gitupdate file

---
+++ gitupdate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ha core check
+
+git add .
+git status
+echo -n "Enter the Description for the Change: " [Minor Update]
+read CHANGE_MSG
+git commit -m "${CHANGE_MSG}"
+git push origin master
+
+exit
